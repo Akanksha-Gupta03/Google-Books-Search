40323e0244d5a44f971259d8285275248dec8f0c
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-08 21:04:00 -0400

    added search section

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,9 +2,15 @@ import React from "react";
 
 function SearchPage(props){
     return(
-        <div >
-            search
+        <div className="container">
+            <form>
+                <div class="form-group">
+                    <label for="search">Search books of your Interest</label>
+                    <input type="text" name="search" class="form-control" id="search" placeholder="Search your Book" />
+                    <button>Search</button>
+                </div>
+            </form>        
         </div>
-    )
+    );
 }
 export default SearchPage;
