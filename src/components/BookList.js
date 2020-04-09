7adb41e6d41a0fe9b5c3e5d9609094d14f5738fb
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-09 12:32:07 -0400

    fixed css for card

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -6,22 +6,30 @@ function BookList (props){
         <div>
             <div className="mb-3">{props.bookList ? props.bookList.map(books=>
              <div className="container">
-                 <div className="bookImage">{books.volumeInfo.imageLinks && books.volumeInfo.imageLinks.thumbnail ?
+                  <div class="card">
+                <div class="row">
+                 <div className="col-md-2 bookImage">{books.volumeInfo.imageLinks && books.volumeInfo.imageLinks.thumbnail ?
                   <img src={books.volumeInfo.imageLinks.thumbnail} alt="" />: "no image" }
              </div>
-             <div className="bookDescription">
+             <div className="col-md-10">
+                
+             <div className=" card-body bookDescription">
                  <h3>{books.volumeInfo.title}</h3>{books.volumeInfo.imageLinks && books.volumeInfo.authors ? 
                  <ul>{books.volumeInfo.authors.map(author =><li>{author}</li>)}</ul> : "no author"}
                  <p>{books.volumeInfo.description}</p>
                  <div class = "d-flex justify-content-start">
-                     <a class = "btn" href={books.volumeInfo.previewLink}>View</a>
+                     <a class = "btn badge-pill btn-outline-dark mt-3" href={books.volumeInfo.previewLink}>View</a>
+                     <a class="btn badge-pill btn-outline-warning mt-3 ml-3" href="/saved" >Saved</a>
                  </div>
-                 </div>
-                 </div>
+                </div>
+                </div>
+                </div>
+              </div>
+            </div>
             ) : "no match"
             }
-             </div>
-            </div>
+         </div>
+        </div>
     )
 }
 export default BookList;
