2e6f5a6b388b573d394a5385936aa742d93b9d38
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-08 20:19:29 -0400

    fixed collapse navbar functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,23 @@
-import React  from 'react';
+import React, { useState }  from 'react';
 import { Link, useLocation } from "react-router-dom";
 import "../style/NavBar.css";
 
 /* activePage  | changePage-call-back */
 function NavBar(props) {
   const location = useLocation();
-//   const [isShown, setShown] = useState(false);
-//   let showClass = !isShown ? `collapse navbar-collapse`: `collapse navbar-collapse show`;
+  const [isShown, setShown] = useState(false);
+  let showClass = !isShown ? `collapse navbar-collapse`: `collapse navbar-collapse show`;
 
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <Link to="/" className="navbar-brand"><strong>Google Books</strong>
       </Link>
       {/* <a class="navbar-brand" href="#" onClick={ function(){ props.changePage('AboutPage')} }>Pupster</a> */}
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={()=>setShown(!isShown)}>
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class={showClass} id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto special">
           <li className="nav-item active">
             <Link to="/search" className={location.pathname === "/search" ? "nav-link active" : "nav-link"}>
