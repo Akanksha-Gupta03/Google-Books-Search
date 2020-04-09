e2c5ed198440e18d69945e9ecf0e19075d5d8f0a
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-08 20:08:35 -0400

    fixed styling for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,16 @@
 import React  from 'react';
 import { Link, useLocation } from "react-router-dom";
+import "../style/NavBar.css";
 
 /* activePage  | changePage-call-back */
 function NavBar(props) {
   const location = useLocation();
 //   const [isShown, setShown] = useState(false);
 //   let showClass = !isShown ? `collapse navbar-collapse`: `collapse navbar-collapse show`;
-  const style = {
-      logo: { width: '64px', height: '64px' }
-  }
 
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="navbar-brand">
-          <img src='https://i.pinimg.com/originals/6e/d2/43/6ed243fcb389252740f7c4c41e07b560.jpg' alt="" style={style.logo} />
+      <Link to="/" className="navbar-brand"><strong>Google Books</strong>
       </Link>
       {/* <a class="navbar-brand" href="#" onClick={ function(){ props.changePage('AboutPage')} }>Pupster</a> */}
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +18,7 @@ function NavBar(props) {
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ml-auto special">
           <li className="nav-item active">
             <Link to="/search" className={location.pathname === "/search" ? "nav-link active" : "nav-link"}>
              <p class="p">Search</p>
