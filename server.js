1ced8402d8ecc28d94d2c721bf8bea2ac62547ad
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-10 10:58:58 -0400

    fixed error server is running

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-require('dotenv').confiq();
-const express = require('express');
+  
+require('dotenv').config(); // --> process.env
+const express = require( 'express' );
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use( express.static('client/build/') );
+app.use( express.urlencoded({ extended: false }) );
 
-app.use(express.static('client/build/'));
-app.use(express.urlencoded({extended: false}));
 
-app.listen(PORT, function(){
-    console.log(`[google-books-search server] RUNNING,http://localhost:${PORT}`);
-})
+app.listen( PORT, function(){
+    console.log( `[everest server] RUNNING, http://localhost:${PORT}` );
+ });
