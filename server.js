ae2f94518c0f857cdfa2e13a7284a2e915292780
Akanksha Gupta <akanksha.rapti@gmail.com>
2020-04-10 09:28:23 -0400

    fixed error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static('client/bulid/'));
+app.use(express.static('client/build/'));
 app.use(express.urlencoded({extended: false}));
 
 app.listen(PORT, function(){
